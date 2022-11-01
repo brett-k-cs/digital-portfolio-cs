0ecbb682d2ba957ad392084d7d247dab9324403d
Brett Kaplan <bkapla01@gmail.com>
2022-11-01 09:31:21 -0500

    Business Plan and Sort Analysis

--- CS-II/Projects/index.php
+++ CS-II/Projects/index.php
@@ -17,15 +17,25 @@
 	  <?php
       	  if ($handle = opendir('.')) {
 	      while (false !== ($entry = readdir($handle))) {
-		 if ($entry != "." && $entry != ".." && strpos($entry, "#") == false && strpos($entry, "~") == false && (strpos($entry, ".txt") != false || strpos($entry, ".html") != false) && (strpos($entry, "index") == false) && (strpos($entry, "Tutorial") == false)) {
+		 if ($entry != "." && $entry != ".." && strpos($entry, "#") == false && strpos($entry, "~") == false && (strpos($entry, ".txt") != false || strpos($entry, ".html") != false) && (strpos($entry, "index") == false && strpos($entry, "/") == false) && (strpos($entry, "Tutorial") == false)) {
 		     list($title, $extension) = explode('.', $entry);
 		     $entry = str_replace(" ", "%20", $entry);
 		    echo "<h2><a href='$entry'>$title</a></h2>";
 		 }
+		  if($entry == "Business Plan") {
+		      echo "<div class=\"dropdown dropBgHover\">";
+		      echo "<button class=\"dropbtn\" style=\"font-weight: bold; text-decoration: underline;\" onmouseover=\"toggleDropdown('Business-Plan')\">Business Plan</button>";
+		      echo "<div id=\"Business-Plan\" class=\"dropdown-content\">";
+		      echo "<a href=\"Business%20Plan/index.html\">Home</a>";
+		      echo "<a href=\"Business%20Plan/considered.html\">Considered Proposals</a>";
+		      echo "</div>";
+		      echo "</div>";
+		  }
 	     }
 	     closedir($handle);
 	 }
       ?>
-    </main>
+      </main>
+      <script src="../../main.js"></script>
   </body>
   </html>
