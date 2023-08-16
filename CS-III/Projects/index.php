463842419cd64f10644448ce13bfc6d594ab5211
Brett Kaplan <bkapla01@gmail.com>
2023-08-16 11:45:19 -0500

    Added CS-III

---
+++ CS-III/Projects/index.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>Brett Kaplan's Digital Portfolio - CS III</title>
+    <meta name="description" content="Digital Portfolio">
+    <link href="../../styles.css" rel="stylesheet" />
+  </head>
+  <body>
+      <nav class="mainNav">
+	  <a href="../index.html">Comp Sci III</a>
+	<a href="../../index.html">Home</a>
+	<a href="../../aboutme.html">About Me</a>
+	<a href="../../projects.html">Projects</a>
+      </nav>
+      <main>
+	  <h1 style="text-align: left;">Computer Science III - Projects</h1>
+	  <?php
+      	  if ($handle = opendir('.')) {
+	      while (false !== ($entry = readdir($handle))) {
+		 if ($entry != "." && $entry != ".." && strpos($entry, "#") == false && strpos($entry, "~") == false && (strpos($entry, ".txt") != false || strpos($entry, ".html") != false) && (strpos($entry, "index") == false && strpos($entry, "/") == false) && (strpos($entry, "Tutorial") == false)) {
+		     list($title, $extension) = explode('.', $entry);
+		     $entry = str_replace(" ", "%20", $entry);
+		    echo "<h2><a href='$entry'>$title</a></h2>";
+		 }
+	     }
+	     closedir($handle);
+	 }
+      ?>
+      </main>
+      <script src="../../main.js"></script>
+  </body>
+  </html>
